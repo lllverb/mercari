450d42cc6017cca2c54cda3fe1294fa35ed9f8a6
lllverb <wktkm9orz2@gmail.com>
2019-10-08 15:26:15 +0900

    new user page3つ

--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,7 @@ Rails.application.routes.draw do
   get 'tests/credit_create'
   get 'tests/credit'
   get 'tests/index'
-  get 'tests/newuser1'
+  get 'tests/newuser3'
 
   resources :products, only: [:index, :show] 
 
