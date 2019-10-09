056e1c76d038005cbe848940b838cfc9f62d398e
una7 <2ne1.jeremy@gmail.com>
2019-10-09 18:23:42 +0900

    update_my_page
    マイページ

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -23,7 +23,7 @@ class UsersController < ApplicationController
   private
 
   def user_params
-    params.require(:user).permit(:name, :email, :nickname)
+    params.require(:user).permit(:nickname, :email)
   end
 
 end
