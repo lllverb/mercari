e585656d1dc8b5b2493488fe97efe3b7b40c8410
lllverb <wktkm9orz2@gmail.com>
2019-10-14 18:41:10 +0900

    install completed

---
+++ app/controllers/signup_controller.rb
@@ -0,0 +1,68 @@
+class SignupController < ApplicationController
+  def step1
+    @user = User.new
+  end
+
+  def step2
+    session[:nickname] = user_params[:nickname]
+    session[:email] = user_params[:email]
+    session[:password] = user_params[:password]
+    session[:password_confirmation] = user_params[:password_confirmation]
+    session[:last_name] = user_params[:last_name]
+    session[:first_name] = user_params[:first_name]
+    session[:last_name_kana] = user_params[:last_name_kana]
+    session[:first_name_kana] = user_params[:first_name_kana]
+    @user = User.new
+  end
+
+  def step3
+    session[:phone_number] = user_params[:phone_number]
+    @user = User.new
+  end
+
+  def step4
+    @user = User.new
+  end
+
+  def create
+    @user = User.new(
+      nickname: session[:nickname],
+      email: session[:email],
+      password: session[:password],
+      password_confirmation: session[:password_confirmation],
+      last_name: session[:last_name],
+      first_name: session[:first_name],
+      last_name_kana: session[:last_name_kana],
+      first_name_kana: session[:first_name_kana],
+      phone_number: session[:phone_number],
+    )
+
+    if @user.save
+      session[:id] = @user.id
+      redirect_to done_signup_index_path
+    else
+      render '/signup/registration'
+    end
+  end
+
+  def done
+    sign_in User.find(session[:id]) unless user_signed_in?
+  end
+
+  private
+  def user_params
+    params.require(:user).permit(
+      :nickname,
+      :email,
+      :password,
+      :password_confirmation,
+      :last_name,
+      :first_name,
+      :last_name_kana,
+      :first_name_kana,
+      :profile_image,
+      :phone_number,
+    )
+  end
+end
+
--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,16 @@ devise_for :users,
   omniauth_callbacks: 'users/omniauth_callbacks'
   }
 
+  resources :signup do
+    collection do
+      get 'step1'
+      get 'step2'
+      get 'step3'
+      get 'step4' # ここで、入力の全てが終了する
+      get 'done' # 登録完了後のページ
+    end
+  end
+
   root 'products#index'
   
 # user関係
