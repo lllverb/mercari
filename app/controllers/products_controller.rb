90ee73c1feb7c382a0154484a1c2960bc3072219
una7 <2ne1.jeremy@gmail.com>
2019-10-18 15:49:26 +0900

    pay.jpを導入いたしました
    payjpを用いたクレジット決済機能の実装を行いました。
    1. PAY.JPのアカウントを作成
    2.payjp gemの導入
    3.フォームを作成する
    4.コントローラーにpayアクションを追加する
    5.payアクションのルーティングを再定義する

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -27,10 +27,24 @@ class ProductsController < ApplicationController
   def edit
   end
 
+  def pay
+    @product = Product.find(params[:id])
+    Payjp.api_key = 'sk_test_742b25b3c7d64d5f154f9605'
+    charge = Payjp::Charge.create(
+    :amount => @product.price,
+    :card => params['payjp-token'],
+    :currency => 'jpy',
+    )
+    redirect_to products_complete_path
+  end
+
+  def complete
+  end
+
 
   private
   def product_params
-    params.require(:product).permit(:name, :detail, :category_id, :condition_id, :shipping_fee_id, :shipping_method_id, :prefecture_id, :deliveryday_id, :price, product_images_attributes:[:id,:image]).merge(user_id: current_user.id)
+    params.require(:product).permit(:name, :detail, :category_id, :condition_id, :shipping_fee_id, :shipping_method_id, :prefecture_id, :deliveryday_id, :price, :pay, product_images_attributes:[:id,:image]).merge(user_id: current_user.id)
   end
 
 end
