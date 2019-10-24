c10daba02322924daa9d919cb21b8057ab4119ec
lllverb <wktkm9orz2@gmail.com>
2019-10-24 12:34:57 +0900

    画像無し投稿不可

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -24,6 +24,7 @@ class ProductsController < ApplicationController
   end
   
   def create
+    binding.pry
     @product = Product.new(product_params)
     if @product.save
       redirect_to root_path
@@ -82,7 +83,19 @@ class ProductsController < ApplicationController
 
   private
   def product_params
-    params.require(:product).permit(:name, :detail, :category_id, :condition_id, :shipping_fee_id, :shipping_method_id, :prefecture_id, :deliveryday_id, :price, :pay, product_images_attributes:[:id, :image]).merge(user_id: current_user.id)
+    params.require(:product)
+    .permit(:name,
+            :detail, 
+            :category_id, 
+            :condition_id, 
+            :shipping_fee_id, 
+            :shipping_method_id, 
+            :prefecture_id, 
+            :deliveryday_id, 
+            :price, 
+            :pay, 
+            product_images_attributes:[:image])
+            .merge(user_id: current_user.id)
   end
 
   def set_product
